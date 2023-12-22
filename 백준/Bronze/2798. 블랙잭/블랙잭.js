f4d84022f9c310197c02fa125404cbb61930924b
YoeungHyeon <yhoung11@gmail.com>
2023-12-22 22:27:18 +0900

    [Bronze II] Title: 블랙잭, Time: 256 ms, Memory: 9676 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -0,0 +1,96 @@
+/*
+블랙잭
+ 
+시간 제한	메모리 제한
+1 초	128 MB	
+문제
+카지노에서 제일 인기 있는 게임 블랙잭의 규칙은 상당히 쉽다. 
+카드의 합이 21을 넘지 않는 한도 내에서, 카드의 합을 최대한 크게 만드는 게임이다. 
+블랙잭은 카지노마다 다양한 규정이 있다.
+
+한국 최고의 블랙잭 고수 김정인은 새로운 블랙잭 규칙을 만들어 상근, 창영이와 게임하려고 한다.
+
+김정인 버전의 블랙잭에서 각 카드에는 양의 정수가 쓰여 있다. 
+그 다음, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다. 그런 후에 딜러는 숫자 M을 크게 외친다.
+
+이제 플레이어는 제한된 시간 안에 N장의 카드 중에서 3장의 카드를 골라야 한다. 
+블랙잭 변형 게임이기 때문에, 플레이어가 고른 카드의 합은 M을 넘지 않으면서 M과 최대한 가깝게 만들어야 한다.
+
+N장의 카드에 써져 있는 숫자가 주어졌을 때, 
+M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
+
+입력
+첫째 줄에 카드의 개수 N(3 ≤ N ≤ 100)과 M(10 ≤ M ≤ 300,000)이 주어진다. 
+둘째 줄에는 카드에 쓰여 있는 수가 주어지며, 이 값은 100,000을 넘지 않는 양의 정수이다.
+
+합이 M을 넘지 않는 카드 3장을 찾을 수 있는 경우만 입력으로 주어진다.
+
+출력
+첫째 줄에 M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 출력한다.
+
+예제 입력 1 
+5 21
+5 6 7 8 9
+예제 출력 1 
+21
+예제 입력 2 
+10 500
+93 181 245 214 315 36 185 138 216 295
+예제 출력 2 
+497
+*/
+
+"use strict";
+// Common Template Starts //
+// 입력 코드
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+
+    main();
+});
+
+function readline() {
+    return inputString[currentLine++];
+}
+// Common Template Ends //
+
+
+function main() {
+    let inputs = readline().split(' ').map(Number);
+
+    const N = inputs[0];
+    const M = inputs[1];
+
+    const arr = readline().split(' ').map(Number);
+
+    let res = 0;
+
+    for (let i = 0; i < N - 2; i++) {
+        for (let j = i + 1; j < N - 1; j++) {
+            for (let k = j + 1; k < N; k++) {
+                let sum = arr[i] + arr[j] + arr[k];
+                if (M == sum) res = sum;
+
+                if(res < sum && sum < M) {
+                    res = sum;
+                }
+            }
+        }
+    }
+    console.log(res);
+};
