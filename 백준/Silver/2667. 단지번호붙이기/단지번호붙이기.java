bbc5c3fbd2cbd4a4002dceeb9034cbe6abeba40f
YoeungHyeon <yhoung11@gmail.com>
2024-02-01 16:33:21 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,97 @@
+/*
+단지번호붙이기
+ 
+시간 제한	메모리 제한
+1 초	128 MB
+문제
+<그림 1>과 같이 정사각형 모양의 지도가 있다. 
+1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다. 
+철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 
+단지에 번호를 붙이려 한다. 여기서 연결되었다는 것은 어떤 집이 좌우, 
+혹은 아래위로 다른 집이 있는 경우를 말한다. 
+대각선상에 집이 있는 경우는 연결된 것이 아니다.
+<그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 
+지도를 입력하여 단지수를 출력하고, 
+각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
+
+입력
+첫 번째 줄에는 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)이 입력되고, 
+그 다음 N줄에는 각각 N개의 자료(0혹은 1)가 입력된다.
+
+출력
+첫 번째 줄에는 총 단지수를 출력하시오. 
+그리고 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력하시오.
+
+예제 입력 1 
+7
+0110100
+0110101
+1110101
+0000111
+0100000
+0111110
+0111000
+예제 출력 1 
+3
+7
+8
+9
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static int n, cnt;
+   static char[][] map;
+   static boolean[][] visited;
+   static int[] X = {0, 0, -1, 1};
+   static int[] Y = {-1, 1, 0, 0};
+   
+   public static void main(String[] args) throws IOException {
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      n = Integer.parseInt(br.readLine());
+      map = new char[n][n];
+      visited = new boolean[n][n];
+      
+      for (int i = 0; i < n; i++) {
+        map[i] = br.readLine().toCharArray();
+      }
+      StringBuilder sb = new StringBuilder();
+      List<Integer> list = new ArrayList<>();
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (map[i][j] == '1' && !visited[i][j]){
+            cnt++;
+            list.add(bfs(i, j));
+          }
+        }
+      }
+      sb.append(cnt).append('\n');
+      list.stream().sorted().forEach(e -> sb.append(e).append('\n'));
+      System.out.print(sb);
+   }
+   
+   static int bfs(int x, int y) {
+     int apart = 1;
+     Queue<int[]> que = new ArrayDeque<>();
+     que.offer(new int[]{x, y});
+     visited[x][y] = true;
+     while(!que.isEmpty()) {
+       int[] node = que.poll();
+       for (int i = 0; i < 4; i++) {
+         int nx = node[0] + X[i];
+         int ny = node[1] + Y[i];
+         if (nx < 0 || nx >= n|| ny < 0 || ny >= n) continue;
+         if (map[nx][ny] == '1' && !visited[nx][ny]) {
+           apart++;
+           que.offer(new int[]{nx, ny});
+           visited[nx][ny] = true;
+         }
+       }
+     }
+     return apart;
+   }
+}
