e355fd797ba4999b6de022e4d14e2c5e015f4b17
YoeungHyeon <yhoung11@gmail.com>
2025-06-05 09:48:07 +0900

    [Bronze IV] Title: A Hero Named Magnus, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31134. A Hero Named Magnus/A Hero Named Magnus.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long x;
+        cin >> x;
+        long long n = 2LL * x - 1;
+        cout << n << "\n";
+    }
+    return 0;
+}
