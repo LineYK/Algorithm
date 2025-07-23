98a2c5bb6eb7431eb91d7033b2278bbd8785ccdb
YoeungHyeon <yhoung11@gmail.com>
2025-07-23 09:17:12 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int A, B;
+    cin >> A >> B;
+    cout << (B - A) << ' ' << B << "\n";
+    return 0;
+}
